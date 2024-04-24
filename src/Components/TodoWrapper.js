658901de67492e64034cec196604f91797456ea6
pottabathinivivekananda <vivekananda.99666@gmail.com>
2024-04-24 18:50:41 +0530

    name

--- src/Components/TodoWrapper.js
+++ src/Components/TodoWrapper.js
@@ -42,7 +42,7 @@ export const TodoWrapper = () => {
 
   return (
     <div className="TodoWrapper">
-      <h1>Todo List!</h1>
+      <h1>OrganizePro!</h1>
       <TodoForm addTodo={addTodo} />
       {/* display todos */}
       {todos.map((todo) =>
